6ad92fb7f541b8a37ce58eac82fe4d602971a06b
Nayara Ribeiro Freire <nayfreireof@gmail.com>
2021-01-26 20:16:02 -0300

    Tentativa de dar push no heroku através da deleção da pasta node_modules falha

--- controller/funcionarios-controller.js
+++ controller/funcionarios-controller.js
@@ -1,5 +1,5 @@
 const mysql = require('../mysql').pool;
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt-nodejs')
 const jwt = require('jsonwebtoken')
 
 exports.postFuncionario = (req, res, next) => {

113c941386a528362954c10cd34e07a52d9038bd
Nayara Ribeiro Freire <nayfreireof@gmail.com>
2021-01-29 12:20:17 -0300

    O erro mudou para App crashe

--- controller/funcionarios-controller.js
+++ controller/funcionarios-controller.js
@@ -1,5 +1,5 @@
 const mysql = require('../mysql').pool;
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 exports.postFuncionario = (req, res, next) => {
